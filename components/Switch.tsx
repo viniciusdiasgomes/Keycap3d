613f94adccb79722c8e4d8a8eca4171edb6ec3e5
Vini Dias <vinidiasgomes2005@gmail.com>
2026-01-15 21:22:15 -0300

    created switch palyground slice

--- components/Switch.tsx
+++ components/Switch.tsx
@@ -1,7 +1,18 @@
 import * as THREE from "three";
-import React from "react";
+import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
+import { ThreeEvent } from "@react-three/fiber";
+import gsap from "gsap"
+
+
+
+export const SOUND_MAP = {
+    red:["/sounds/red-1.mp3", "/sounds/red-2.mp3", "/sounds/red-3.mp3",],
+    brown:["/sounds/brown-1.mp3", "/sounds/brown-2.mp3", "/sounds/brown-3.mp3",],
+    blue:["/sounds/blue-1.mp3", "/sounds/blue-2.mp3", "/sounds/blue-3.mp3",],
+    black:["/sounds/black-1.mp3", "/sounds/black-2.mp3", "/sounds/black-3.mp3",],
+}
 
 // Type definitions
 type GLTFResult = GLTF & {
@@ -14,7 +25,7 @@ type GLTFResult = GLTF & {
   materials: Record<string, unknown>;
 };
 
-
+ 
 type SwitchProps = React.ComponentProps<"group"> & {
     color:"red"| "brown"| "blue"| "black";
     hexColor: string;
@@ -23,15 +34,136 @@ type SwitchProps = React.ComponentProps<"group"> & {
 
 export function Switch({color, hexColor, ...restProps}: SwitchProps) {
   const { nodes } = useGLTF("/switch.gltf") as unknown as GLTFResult;
+  const switchGroupRef = useRef<THREE.Group>(null)
+  const stemRef = useRef<THREE.Mesh>(null)
+  const isPressedRef = useRef(false)
+  const audio = useRef<HTMLAudioElement>(null)
+  const audioTimeout = useRef<ReturnType<typeof setTimeout>>(null)
+  const allAudio = useRef(
+    SOUND_MAP[color].map((url)=>{
+        const audio = new Audio(url);
+        audio.volume = 0.6;
+        return audio;
+    })
+  )
+
+
+
+
+  const handlePointerDown = (event: ThreeEvent<PointerEvent>)=> {
+    event.stopPropagation()
+
+
+    if (!stemRef.current || !switchGroupRef.current || isPressedRef.current) return 
+    isPressedRef.current = true;
+
+
+    const stem = stemRef.current
+    const switchGroup = switchGroupRef.current
+
+
+    gsap.killTweensOf(stem.position)
+    gsap.killTweensOf(switchGroup.rotation)
+
+
+
+    gsap.to(switchGroup.rotation, {
+        
+        x: Math.PI / 2 + 0.1,
+        duration: 0.05, 
+        ease:"power2.out"
+
+    })
+
+
+
+    gsap.to(stem.position, {
+      z:0.005,
+      duration:0.08,
+      ease:"power2.out"
+    })
+
+
+// Audio
+audio.current = gsap.utils.random(allAudio.current)
+audio.current.currentTime = 0
+audio.current.play()
+audioTimeout.current = setTimeout(()=> audio.current?.parse(), (audio.current.duration / 2) *  1000,)
+
+
+
+  }
+
+
+  const releaseSwitch = ()=> {
+
+
+
+    if (!stemRef.current || switchGroupRef.current || !isPressedRef.current) return 
+    isPressedRef.current = false;
+
+
+    const stem = stemRef.current
+    const switchGroup = switchGroupRef.current
+
+    gsap.to(switchGroup.rotation, {
+        
+        x: Math.PI / 2,
+        duration: 0.6, 
+        ease:"elastic.out(1,0,3)",
+
+    })
+
+
+
+
+    gsap.to(stem.position, {
+      z:0,
+      duration:0.15,
+      ease:"elastic.out(1, 0.3)",
+
+    })
+
+
+if(audioTimeout.current) clearTimeout(audioTimeout.current)
+    audio.current?.play();
+
+
+  }
+
+
+const handlePointerUp  = (event: ThreeEvent<PointerEvent>)=> {  
+    event.stopPropagation();
+    releaseSwitch()
+}
+
+
+const handlePointerLeave = ()=>{
+    releaseSwitch()
+}
+
+
 
   return (
 
     <group{...restProps}>
 
+<mesh
+position={[0,0.05, 0]}
+onPointerDown={handlePointerDown}
+onPointerUp={handlePointerUp}
+onPointerLeave={handlePointerLeave}
+onPointerOver={()=>(document.body.style.cursor = "pointer")}
+onPointerOut={()=>(document.body.style.cursor = "default")}
+
+>
+    <boxGeometry args={[0.15, 0.15, 0.15]}/>
+    <meshBasicMaterial transparent opacity={0} />
+    </mesh>
 
 
 
-    <group scale={10} rotation={[Math.PI / 2, 0, 0]}>
+    <group ref={switchGroupRef} scale={10} rotation={[Math.PI / 2, 0, 0]}>
       {/* Switch housing */}
       <mesh
         castShadow
@@ -52,6 +184,7 @@ export function Switch({color, hexColor, ...restProps}: SwitchProps) {
 
       {/* Colored stem */}
       <mesh
+       ref={stemRef}
         castShadow
         receiveShadow
         geometry={nodes.Single_Switch_Mesh_3.geometry}
--- slices/SwitchPlayground/index.tsx
+++ slices/SwitchPlayground/index.tsx
@@ -9,7 +9,8 @@ import clsx from "clsx";
 import { Canvas } from "@react-three/fiber";
 import { Switch } from "@/components/Switch";
 import { Stage } from "@react-three/drei";
-
+import gsap from 'gsap'
+import { LuVolume2 } from "react-icons/lu";
 export type SwitchPlaygroundProps =
   SliceComponentProps<Content.SwitchPlaygroundSlice>;
 
@@ -76,8 +77,25 @@ const SharedCanvas = ({color}: ShareCanvasProps)=>{
     black: "bg-gray-900",
   }[colorName]
 
+
+  const handleSound = ()=>{
+    const selectedSound = gsap.utils.random(SOUND_MAP[colorName])
+
+    const audio = new Audio(selectedSound);
+    audio.volume = 0.6;
+    audio.play();
+  }
+
   return (
     <div className="group relative min-h-96 overflow-hidden rounded-3xl select-none">
+
+{/* texto button */}
+<button 
+onClick={handleSound}
+className="font-bold-slanted absolute bottom-0 left-0 z-10 fflex items-center gap-3 p-6 text-4xl text-white uppercase focus:ring-2 focus:ring-white focus:outline-none">
+  {name} <LuVolume2 />
+</button>
+
 
       <Canvas camera={{position:[1.5, 2, 0], fov: 7}}>
         <Stage adjustCamera intensity={0.5} shadows={"cotact"} Environment="city">
